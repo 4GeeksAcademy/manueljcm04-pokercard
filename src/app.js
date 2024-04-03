a2b6e354de8a1dfbaefca428252a70e6944ce89e
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-04-03 15:47:17 +0000

    poker

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+const ramdonGenerate = Array => {
+  const randomIndex = Math.floor(Math.random() * Array.length);
+  return randomIndex;
+};
+window.onload = function() {
+  const palos = [":diamantes:", ":corazones:", ":picas:", ":tréboles:"];
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const ramdonpalo = palos[ramdonGenerate(palos)];
+  console.log(ramdonGenerate(palos));
+  console.log(palos.length);
+  const ramdonnumero = numeros[ramdonGenerate(numeros)];
+  console.log(ramdonnumero);
+  console.log("Hello Rigo from the console!");
+};
